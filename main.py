94c1264d609aee8be9006c8c93cdfe44cdc58cc7
seamar01 <mark.seaman@csgi.com>
2022-07-14 09:22:11 -0600

    fast api

--- main.py
+++ main.py
@@ -1,1 +1,64 @@
-# Put the code for your API here.
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+from model_train.base import model
+
+class InputDate(BaseModel):
+    age: float
+    workclass: str
+    fnlgt: float
+    education: str
+    education_num: float
+    marital_status: str
+    occupation: str
+    relationship: str
+    race: str
+    sex: str
+    capital_gain: float
+    capital_loss: float
+    hours_per_week: float
+    native_country: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "age": 39,
+                "workclass": "State-gov",
+                "fnlgt": 77516,
+                "education": "Bachelors",
+                "education_num": 13,
+                "marital_status": "Never-married",
+                "occupation": "Adm-clerical",
+                "relationship": "Not-in-family",
+                "race": "White",
+                "sex": "Male",
+                "capital_gain": 2174,
+                "capital_loss": 0,
+                "hours_per_week": 40,
+                "native_country": "United-States",
+            }
+        }
+
+
+app = FastAPI()
+
+@app.get("/")
+async def welcome():
+    return {"message": "This is a FAST API GBM deploymenet. Use this app to predict income."}
+
+@app.post("/predict")
+async def predict(data: InputData):
+    input = jsonable_encoder(data)
+    df = pd.DataFrame(input, index=[0])
+
+    with open('model/model.pkl', 'rb') as f:
+        output_model = pickle.load(f)
+    with open('../model/encoder.pkl', 'rb') as f:
+        encoder = pickle.load(f)
+    with open('../model/lb.pkl', 'rb') as f:
+        lb = pickle.load(f)
+
+    X_test, y_test, _, _ = process_data(test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb)
+
+    predictions = model.inference(output_model, X_test)
+    return predictions
--- model_card_template.md
+++ model_card_template.md
@@ -3,16 +3,24 @@
 For additional information see the Model Card paper: https://arxiv.org/pdf/1810.03993.pdf
 
 ## Model Details
+GBM classifier using slightly tuned parameters to predict census data.
 
 ## Intended Use
+This model helps provide insight into income based on a number of factor
 
 ## Training Data
+80% - nulls are removed and rest of data is clean
 
 ## Evaluation Data
+20% split from original data
 
 ## Metrics
 _Please include the metrics used and your model's performance on those metrics._
+Recall, Precision, and F-score are logged
+prec:0.7787979966611018, recall:0.6078175895765472, fbeta:0.6827661909989022
 
 ## Ethical Considerations
+Race and ethnicity may need further investigation on their bias.
 
 ## Caveats and Recommendations
+Model can be improved through more tunning
